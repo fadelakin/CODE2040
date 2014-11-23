b1006eebb0fa0fd2b5dd1f1d3e8505ca5652fd89
Fisher Adelakin <temidayoadelakin@gmail.com>
2014-11-23 14:47:13 -0500

    it compiles but throws error code 200

--- Registration.java
+++ Registration.java
@@ -1,4 +1,46 @@
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class Registration {
+
+	public static void main(String args[]) {
+		try {
+			URL url = new URL("http://challenge.code2040.org/api/register");
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setDoOutput(true);
+			conn.setRequestMethod("POST");
+			conn.setRequestProperty("Content-Type", "application/json");
+
+			String input = "{\"email\":\"temidayoadelakin@gmail.com\",\"github\":\"https://github.com/fadelakin/CODE2040\"}";
+			System.out.println(input);
+
+			OutputStream os = conn.getOutputStream();
+			os.write(input.getBytes());
+			os.flush();
+
+			if(conn.getResponseCode() != HttpURLConnection.HTTP_CREATED) {
+				throw new RuntimeException("Failed : HTTP error code : " + conn.getResponseCode());
+			}
+
+			BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
+
+			String output;
+			System.out.println("Output from server.... \n");
+			while((output = br.readLine()) != null) {
+				System.out.println(output);
+			}
+
+			conn.disconnect();
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
 }
