33419c326559d7ca4ab53a12b504bd93d0392c18
Fisher Adelakin <temidayoadelakin@gmail.com>
2014-11-23 16:33:56 -0500

    Stage 1: Reverse a String

--- Stage1/Reverse.java
+++ Stage1/Reverse.java
@@ -1,3 +1,80 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class Reverse {
-	
+
+	public static void main(String args[]) {
+
+		final String token = "{\"token\":\"sNbVpHJVKL\"}";
+
+		HttpURLConnection conn;
+		HttpURLConnection conntwo;
+
+		OutputStream os;
+		OutputStream ostwo;
+
+		BufferedReader br;
+		BufferedReader brtwo;
+
+		try {
+
+			URL url = new URL("http://challenge.code2040.org/api/getstring");
+			
+			conn = (HttpURLConnection) url.openConnection();
+			conn.setDoOutput(true);
+			conn.setRequestMethod("POST");
+			conn.setRequestProperty("Content-Type", "application/json");
+
+			os = conn.getOutputStream();
+			os.write(token.getBytes());
+			os.flush();
+
+			br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
+
+			String output;
+			System.out.println("Output from server.... \n");
+			while((output = br.readLine()) != null) {
+				System.out.println(output);
+
+				String sub = output.substring(11, 16);
+				System.out.println(sub);
+
+				String reversed = new StringBuffer(sub).reverse().toString();
+				String validate = "{\"token\":\"sNbVpHJVKL\",\"string\":\"" + reversed + "\"}";
+				System.out.println(validate);
+
+				URL validateURL = new URL("http://challenge.code2040.org/api/validatestring");
+
+				conntwo = (HttpURLConnection) validateURL.openConnection();
+				conntwo.setDoOutput(true);
+				conntwo.setRequestMethod("POST");
+				conntwo.setRequestProperty("Content-Type", "application/json");
+
+				ostwo = conntwo.getOutputStream();
+				ostwo.write(validate.getBytes());
+				ostwo.flush();
+
+				brtwo = new BufferedReader(new InputStreamReader((conntwo.getInputStream())));
+
+				String new_output;
+				System.out.println("Output from server.... \n");
+				while((new_output = brtwo.readLine()) != null) {
+					System.out.println(new_output);
+				}
+
+				conntwo.disconnect();
+			}
+
+			conn.disconnect();
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
